6aabd93d1059b2787c9e80e6b9075cee6eff4c2d
Angelo Scalabroni <armotek5421@msn.com>
2015-03-11 14:46:31 -0400

    fix migration & form submit

--- database/migrations/2015_03_10_141556_create_articles_table.php
+++ database/migrations/2015_03_10_141556_create_articles_table.php
@@ -16,7 +16,7 @@ class CreateArticlesTable extends Migration {
 		{
 			$table->increments('id');
                         $table->string('title');
-                        $table->integer('user_id', 10)->unsigned();
+                        $table->integer('user_id', false, true);
                         $table->text('body');
                         $table->timestamp('published_at');
 			$table->timestamps();
